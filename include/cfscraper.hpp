36b99ba587d35fb23c88ecc95c58b1e86714f99a
sandpi3 <laialex13123@gmail.com>
2017-12-01 01:01:48 -0500

    Update and rename cfscraper to cfscraper.hpp

---
+++ include/cfscraper.hpp
@@ -0,0 +1,28 @@
+#include "duktape.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <string>
+#include <regex>
+#include <chrono>
+#include <thread>
+#include "cpr/cpr.h"
+
+class jsResponse{
+public:
+    jsResponse();
+    ~jsResponse();
+    cpr::Response get(std::string url, cpr::Header originalHeaders = cpr::Header());
+    
+private:
+    bool setup(std::string html);    
+    std::string getNewLoc(std::string url);  
+    int answer;
+    int domainLength;
+    cpr::Header headers;
+    int getAnswer();    
+    std::string userAgent;
+    std::string question;
+    std::string jschl_vc;
+    std::string pass;
+
+};
